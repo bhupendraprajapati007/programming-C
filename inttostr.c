7d0660fdc9c8a9b775d2a487e8188b1cd9d019ad
bhupendraprajapati007 <bhupendraprajapati007@gmail.com>
2016-11-14 00:22:47 +0530

    some error resolve like 0 to 19

--- inttostr.c
+++ inttostr.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 void main()
 {
-	int i;
+	int i=0,k;
 	printf("enter the value\n");
-	scanf("%d",&i);
-	
+	scanf("%d",&k);
+	i=k;
 	char arr[20][10]={"zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","sevente	  en","eighteen","nineteen"};	
 	char arr1[10][10]={"","","twenty", "thirty","forty","fifty","sixty","seventy","eighty","ninety","hundred"};
 	int num[2],j=0;
@@ -15,6 +15,9 @@ void main()
 		num[j++]=i%10;
 		i/=10;
 	}
+	if(num[1]==1)
+	printf("%s\n",arr[k]);
+	else
 	printf("%s %s",arr1[num[1]],arr[num[0]]);
 }
 	
